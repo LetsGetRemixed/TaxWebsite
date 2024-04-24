94104177726d135ee8533a787e6ff15f974c6b38
Isaiah Gage <frb32@txstate.edu>
2024-04-24 14:47:43 -0500

    Linked FQ page to routes

--- taxwebsite/src/App.js
+++ taxwebsite/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage'; // Make sure the path matches where your file is located
+import FAQ from './Pages/FAQ';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
+                <Route path='/faq' element={<FAQ />} />
             </Routes>
         </Router>
     );
