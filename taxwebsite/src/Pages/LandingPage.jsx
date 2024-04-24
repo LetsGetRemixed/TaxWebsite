a7ce8c300a4e05f50f58c8ba5162a733360355d0
LetsGetRemixed <73035961+LetsGetRemixed@users.noreply.github.com>
2024-04-23 23:00:09 -0500

    Landing Page
    Landing Page

---
+++ taxwebsite/src/Pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px' }}>
+            <h1>Tax Website</h1>
+            <p>it's grind time boys</p>
+        </div>
+    );
+}
+
+export default LandingPage;
