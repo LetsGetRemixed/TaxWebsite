1868ffd1e252ba91587ded3d758cb7554b93c0d1
Isaiah Gage <frb32@txstate.edu>
2024-04-24 15:53:55 -0500

    Created FAQ css file and created faq QnA

--- taxwebsite/src/Pages/FAQ.jsx
+++ taxwebsite/src/Pages/FAQ.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+import '../css/FAQ.css';
 
 const FAQ = () => {
+    const [openQuestion, setOpenQuestion] = useState([]);
+    const toggleQuestion = (index) => {
+        // Show FAQ questions and answers that are open 
+        if (openQuestion.includes(index)) {
+            setOpenQuestion(openQuestion.filter((item) => item !== index));
+        } else {
+            setOpenQuestion([...openQuestion, index]);
+        }
+    };
+    const faqData = [
+        {
+            question: 'Does you service need protection?',
+            answer: 'Yes'
+        },
+        {
+            question: 'Example',
+            answer: 'No'
+        }
+    ]
   return (
-    <div>FAQ</div>
+    <div>
+        <div>
+            <h2>Frequently Asked Questions</h2>
+            <a href='#'>Support@cleansupport.expert</a>
+        </div>
+        <div className='faq-container'>
+            {
+                faqData.map((faq, index) => (
+                    <div key={index} className='faq-item'>
+                        <div
+                            className='question'
+                            onClick={() => toggleQuestion(index)}
+                            style={{ cursor: 'pointer' }}
+                        >
+                            <div className='question-text'>{faq.question}</div>
+                            <div className='arrow'>{openQuestion.includes(index) ? '▼' : '▶'}</div>
+                        </div>
+                        {openQuestion.includes(index) && <div className='answer'> {faq.answer} </div>}
+                    </div>
+                ))
+            }
+        </div>
+    </div>
   )
 }
 
