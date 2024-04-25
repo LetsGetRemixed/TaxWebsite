605adff17a0a447020e8ea9f920a9062cc16648d
ColbyPerson <cjp187@txstate.edu>
2024-04-25 17:23:14 -0500

    TaxCalculator Creation
    Creation of new page

--- taxwebsite/src/App.js
+++ taxwebsite/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage'; // Make sure the path matches where your file is located
 import FAQ from './Pages/FAQ';
+import Taxcalculator from './TaxCalculator/Taxcalculator';
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<LandingPage />} />
                 <Route path='/faq' element={<FAQ />} />
+                <Route path= '/taxcalc' element = {<Taxcalculator/>} />
             </Routes>
         </Router>
     );
---
+++ taxwebsite/src/TaxCalculator/Taxcalculator.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../Pages/Navbar';
+
+const Taxcalculator = () => {
+    return (
+            <div>
+                < Navbar/>
+                Tax Calculator
+            </div>
+
+
+    );
+};
+
+export default Taxcalculator;
