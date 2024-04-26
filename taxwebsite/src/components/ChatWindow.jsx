3790d854c96fbf7c1583c2bd24c1f5b2c026484b
Isaiah Gage <frb32@txstate.edu>
2024-04-25 23:57:22 -0500

    Added user messages display and questions

--- taxwebsite/src/components/ChatWindow.jsx
+++ taxwebsite/src/components/ChatWindow.jsx
@@ -4,6 +4,13 @@ import '../css/ChatWindow.css'; // Import CSS for styling
 
 const ChatWindow = ({ isOpen, onClose }) => {
   const [userMessage, setUserMessage] = useState('');
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const questions = [
+    "Let me know if you have any questions!",
+    "Hi! Please enter your question and don't leave out any details. This will help get your question fully answered as soon as possible.",
+    "Thanks! Your message has been submitted. We'll get back to you here or via email. We'll respond as soon as we can"
+  ];
+  const [messages, setMessages] = useState([questions[questionIndex]]);
 
   const handleUserMessageChange = (e) => {
     setUserMessage(e.target.value);
@@ -11,7 +18,15 @@ const ChatWindow = ({ isOpen, onClose }) => {
 
   const handleUserMessageSubmit = () => {
     console.log("User Message:", userMessage);
+    const newMessages = [...messages, userMessage]; // Add user message 
+    setMessages(newMessages); // Update messages array adding the user message to it
     setUserMessage('');
+    if (questionIndex < questions.length - 1) {
+      // If there are no more questions, display the next one
+      setQuestionIndex(prevIndex => prevIndex + 1);
+      // Add the next question to the array of messages to display
+      setMessages(prevMessages => [...prevMessages, questions[questionIndex + 1]]); // Update messages array
+    }
   };
 
   return (
@@ -20,12 +35,17 @@ const ChatWindow = ({ isOpen, onClose }) => {
         <button className='close-button' onClick={onClose}>X</button>
       </div>
       <div className='chat-body'>
-        <p>Questions will go here</p>
+        {
+          // Display current question
+          messages.map((message, index) => (
+            <div key={index}> { message } </div>
+          ))
+        }
         <input 
           type='text'
           value={userMessage}
           onChange={handleUserMessageChange}
-          placeholder='Type your response...'
+          placeholder='Enter your question or message here'
         />
         <button onClick={handleUserMessageSubmit}>Send</button>
       </div>
