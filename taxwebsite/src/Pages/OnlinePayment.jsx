5dcef3f5b4d0660e8d60912d2288fbfb687d6f49
ColbyPerson <cjp187@txstate.edu>
2024-04-25 22:18:04 -0500

    Alot better styling choices
    much better styling choices

--- taxwebsite/src/Pages/OnlinePayment.jsx
+++ taxwebsite/src/Pages/OnlinePayment.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import '../css/landingcss.css';
 
 const OnlinePayment = () => {
-    return (
-      <div className="online-payment">
-        <h2>Online Payments</h2>
-        <p>Let your visitors make an online payment or contribute to a fundraiser.</p>
-        <button>Pay Now</button>
+  return (
+      <div className="online-payment-section">
+          <div className="online-payment-image">
+              <img src="/images/content4.png" alt="Online Payment" />
+          </div>
+          <div className="online-payment-content">
+              <h2>Online Payments</h2>
+              <p>Let your visitors make an online payment or contribute to a fundraiser.</p>
+              <button>Pay Now</button>
+          </div>
       </div>
-    );
-  };
-  
+  );
+};
+
   export default OnlinePayment;
