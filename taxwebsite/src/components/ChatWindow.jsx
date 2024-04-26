86290a89202b989d1826644b5e542d6b24921fb2
Isaiah Gage <frb32@txstate.edu>
2024-04-26 00:09:42 -0500

    Added left and rigth align for messages

--- taxwebsite/src/components/ChatWindow.jsx
+++ taxwebsite/src/components/ChatWindow.jsx
@@ -5,12 +5,15 @@ import '../css/ChatWindow.css'; // Import CSS for styling
 const ChatWindow = ({ isOpen, onClose }) => {
   const [userMessage, setUserMessage] = useState('');
   const [questionIndex, setQuestionIndex] = useState(0);
+  
   const questions = [
     "Let me know if you have any questions!",
     "Hi! Please enter your question and don't leave out any details. This will help get your question fully answered as soon as possible.",
     "Thanks! Your message has been submitted. We'll get back to you here or via email. We'll respond as soon as we can"
   ];
-  const [messages, setMessages] = useState([questions[questionIndex]]);
+
+  const [messages, setMessages] = useState([{ text: questions[questionIndex], type: 'question' }]);
+
 
   const handleUserMessageChange = (e) => {
     setUserMessage(e.target.value);
@@ -18,14 +21,14 @@ const ChatWindow = ({ isOpen, onClose }) => {
 
   const handleUserMessageSubmit = () => {
     console.log("User Message:", userMessage);
-    const newMessages = [...messages, userMessage]; // Add user message 
+    const newMessages = [...messages, { text: userMessage, type: 'user' }]; // Add user message 
     setMessages(newMessages); // Update messages array adding the user message to it
     setUserMessage('');
     if (questionIndex < questions.length - 1) {
       // If there are no more questions, display the next one
       setQuestionIndex(prevIndex => prevIndex + 1);
       // Add the next question to the array of messages to display
-      setMessages(prevMessages => [...prevMessages, questions[questionIndex + 1]]); // Update messages array
+      setMessages(prevMessages => [...prevMessages, { text: questions[questionIndex + 1], type: 'question' }]); // Update messages array
     }
   };
 
@@ -38,7 +41,12 @@ const ChatWindow = ({ isOpen, onClose }) => {
         {
           // Display current question
           messages.map((message, index) => (
-            <div key={index}> { message } </div>
+            <div 
+            key={index}
+            className={message.type === 'user' ? 'message user-message' : 'message question-message'}
+            > 
+              { message.text } 
+            </div>
           ))
         }
         <input 
--- taxwebsite/src/components/Message.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Message = ({ text }) => {
-  return (
-    <div>{ text }</div>
-  )
-}
-
-export default Message
